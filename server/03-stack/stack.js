7fea555346b4c8f0317f643083580712b82dd3f3
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-13 15:56:17 +0100

    clean code

--- server/03-stack/stack.js
+++ server/03-stack/stack.js
@@ -12,21 +12,21 @@ function Stack() {
         if (max < 0) {
             throw new Error('capacity cannot be negative');
         }
-        maxItems = max
+        maxItems = max;
     };
 
     this.push = (item) => {
         if ((data.length + 1) > maxItems) {
             throw new Error(`cannot push, stack at capacity limit: ${maxItems}`);
         }
-        data.push(item)
+        data.push(item);
     };
 
     this.pop = () => {
         if (data.length === 0) {
             throw new Error('cannot pop, stack is empty');
         }
-        return data.pop()
+        return data.pop();
     };
 }
 
