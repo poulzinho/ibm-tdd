2ffc77a4313f59857e95d67a320712a48dc0aa90
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-15 17:06:45 +0100

    next version of primer numbers

--- server/04-prime-factors/prime-factors.js
+++ server/04-prime-factors/prime-factors.js
@@ -1,3 +1,6 @@
-const findPrimeFactors = () => [];
+const findPrimeFactors = (number) => {
+    if (number === 1) return [];
+    return [number];
+};
 
 module.exports = {findPrimeFactors};
--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -9,7 +9,9 @@ describe('a prime factors function should', () => {
   it('return none for 1', () => {
     expect(findPrimeFactors(1)).toEqual([]);
   });
-  it.todo('return 2 for 2');
+  it('return 2 for 2', () => {
+    expect(findPrimeFactors(2)).toEqual([2]);
+  });
   it.todo('return 3 for 3');
   it.todo('return 2, 2 for 4');
   it.todo('return 5 for 5');
