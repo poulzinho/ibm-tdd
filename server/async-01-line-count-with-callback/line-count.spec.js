87db2e8c8ed98ffe21dded78d4d386d1b1a51c69
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-17 19:40:41 +0100

    pass first test for async line count

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,8 +1,13 @@
 // eslint-disable-next-line no-unused-vars
+const {lineCount} = require("./line-count");
 const filename = 'server/async-01-line-count-with-callback/line-count.js';
 
 describe('line count', () => {
-  it.todo('for file —— line-count.js —— should be 12');
+  it('for file —— line-count.js —— should be 12', async () => {
+      lineCount(filename, null, (count) => {
+          expect(count).toEqual(12);
+      })
+  });
 
   it.todo('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
