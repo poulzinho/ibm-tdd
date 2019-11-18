3c2132a1502537ce139c72c38e6a55e27b5e9f95
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-18 21:53:57 +0100

    second async test for line count

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -3,12 +3,22 @@ const {lineCount} = require("./line-count");
 const filename = 'server/async-01-line-count-with-callback/line-count.js';
 
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 12', async () => {
-      lineCount(filename, null, (count) => {
-          expect(count).toEqual(12);
-      })
-  });
+    it('for file —— line-count.js —— should be 12', async () => {
+        lineCount(filename, null, (count) => {
+            expect(count).toEqual(12);
+        })
+    });
 
-  it.todo('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+    it('for file —— is-not-there.js —— should be problem reading file: ' +
+        'is-not-there.js', async () => {
+        lineCount(
+            filename,
+            (errMsg) => {
+                expect(errMsg).toEqual('problem reading file: is-not-there.js');
+            },
+            (count) => {
+                expect(count).toBe(12);
+            }
+        )
+    });
 });
