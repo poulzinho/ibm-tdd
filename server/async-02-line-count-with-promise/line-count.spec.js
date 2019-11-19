67a71c13f47d0b7b610f1d5d1c6103e0b5fb5555
Paul Gualotuna <paul.gualotuna.dev@gmail.com>
2019-11-19 21:44:01 +0100

    test promise with done callback

--- server/async-02-line-count-with-promise/line-count.spec.js
+++ server/async-02-line-count-with-promise/line-count.spec.js
@@ -1,12 +1,18 @@
 // eslint-disable-next-line no-unused-vars
+const {lineCount} = require("./line-count");
 const filename = 'server/async-01-line-count-with-callback/line-count.js';
 
 describe('line count can be checked', () => {
-  it.todo('with the done parameter like with a callback');
+    it('with the done parameter like with a callback', (done) => {
+        lineCount(filename).then((count) => {
+            expect(count).toEqual(12);
+        });
+        done();
+    });
 
-  it.todo('by returning the promise after .then');
+    it.todo('by returning the promise after .then');
 
-  it.todo('by using resolves');
+    it.todo('by using resolves');
 
-  it.todo('by using async/await');
+    it.todo('by using async/await');
 });
